952bd8e05c112309573c6479f34604dca70c2f11
Ryan Horrmann <506908@student.fontys.nl>
2023-11-14 23:58:52 +0100

    confirmation message

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -30,7 +30,7 @@ export const ContactForm = () => {
 
   return (
     <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-8">
-     {alert && <div className="alert alert-success bg-primary">
+     {alert && <div className="alert rounded border-none alert-success flex bg-primary">
         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         <span>Message has been sent!</span>
       </div>}
