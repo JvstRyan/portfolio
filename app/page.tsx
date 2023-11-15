e2defb728d49412632bac273e606a92eb0cd8c19
Ryan Horrmann <506908@student.fontys.nl>
2023-11-15 20:19:39 +0100

    components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import Slider from './components/SliderAbout'
-import About from './components/About'
-import SliderDown from './components/SliderTech'
-import Technologies from './components/Technologies'
-import Projects from './components/Projects'
-import SliderProjects from './components/SliderProjects'
-import SlidingContact from './components/SlidingContact'
-import Contact from './components/Contact'
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import NavBar from "@/components/NavBar";
+import Projects from "@/components/Projects";
+import Slider from "@/components/SliderAbout";
+import SliderProjects from "@/components/SliderProjects";
+import SliderDown from "@/components/SliderTech";
+import SlidingContact from "@/components/SlidingContact";
+import Technologies from "@/components/Technologies";
+
 
 export default function Home() {
   return (
