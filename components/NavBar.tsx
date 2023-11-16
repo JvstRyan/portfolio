58f7fe493879bcd19afc3e36c8bcade89708f4a6
Ryan Horrmann <ryanhorrmann123@gmail.com>
2023-11-16 11:13:36 +0100

    Updating Links

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,11 +16,11 @@ const NavBar = () => {
                 <p className='text-black font-bold ml-10'>Ryanhorrmann.</p>
             </article>
             <ul className='flex-1 flex navbar-end'>
-                <Link href={'#about'} className='mr-10 hidden lg:block text-black font-bold text-sm '>About</Link>
-                <Link href={'#tech'} className='mr-10 hidden lg:block text-black font-bold text-sm '>Technologies</Link>
-                <Link href={'#projects'}  className='mr-10 hidden lg:block text-black font-bold text-sm '>Project</Link>
-                <a href={'#contact'}>
-                  <button  className='mr-10 hidden md:flex  btn btn-secondary text-white font-bold text-xs'>Contact<BsArrowRight/></button>
+                <a href='#about' className='mr-10 hidden lg:block text-black font-bold text-sm '>About</a>
+                <a href='#tech' className='mr-10 hidden lg:block text-black font-bold text-sm '>Technologies</a>
+                <a href='#projects' className='mr-10 hidden lg:block text-black font-bold text-sm '>Project</a>
+                <a href='#contact'>
+                  <button  className='mr-10 hidden md:flex btn btn-secondary text-white font-bold text-xs'>Contact<BsArrowRight/></button>
                 </a>
                 <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/ryan-horrmann-1b30b1255/' className='mr-4 '><ImLinkedin color='black' fontSize='1.5rem'/></a>
                 <a target='_blank' rel='noopener noreferrer' href='https://github.com/JvstRyan?tab=repositories' className='mr-10 '><BsGithub color='black' fontSize='1.5rem'/></a>
