15455dacbf3bb7890af96d9ba4d34eea4c367224
Ryan Horrmann <ryanhorrmann123@gmail.com>
2024-03-30 13:21:05 +0100

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Hero from "@/components/Hero";
-import NavBar from "@/components/NavBar";
-import Projects from "@/components/Projects";
-import Slider from "@/components/SliderAbout";
-import SliderProjects from "@/components/SliderProjects";
-import SliderDown from "@/components/SliderTech";
-import SlidingContact from "@/components/SlidingContact";
-import Technologies from "@/components/Technologies";
+import About from "@/app/components/About";
+import Contact from "@/app/components/Contact";
+import Hero from "@/app/components/Hero";
+import NavBar from "@/app/components/NavBar";
+import Projects from "@/app/components/Projects";
+import Slider from "@/app/components/SliderAbout";
+import SliderProjects from "@/app/components/SliderProjects";
+import SliderDown from "@/app/components/SliderTech";
+import SlidingContact from "@/app/components/SlidingContact";
+import Technologies from "@/app/components/Technologies";
 
 
 export default function Home() {
